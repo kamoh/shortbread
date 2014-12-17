22f02384c57fee14c3a6e33d6270159ef384705c
kamoh <ben.serviss@gmail.com>
2014-12-16 21:47:02 -0500

    Adds retrieve action on LinksController to retrieve original URLs when using the shortened URL

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,20 @@ class LinksController < ApplicationController
 
 require 'pry'
 
+  def retrieve
+    if @link = Link.find_by(short_url: params[:unknown])
+      forward(@link)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def forward(link)
+    link.times_visited += 1
+    link.save!
+    redirect_to link.original_url
+  end
+
   def new
     @link = Link.new
     @links = Link.all
