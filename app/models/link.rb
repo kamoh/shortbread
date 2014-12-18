d501747abfb581121eadfd55a48b1ee2f6e7edf7
kamoh <ben.serviss@gmail.com>
2014-12-18 03:23:14 -0500

    Extracts upper limit on top 100 (or any other #) visited links into a constant in link.rb

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ActiveRecord::Base
 
 URL_BASE = "shrtb.red/"
+MOST_VISITED_LIMIT = 100
 
 validates_presence_of :original_url
 
@@ -12,32 +13,24 @@ validates :original_url, format: {
   def create_short_url(link)
     link = sanitize_link(link)
     keygen_source = ('a'..'z').to_a.zip('A'..'Z').to_a.zip(1..9).flatten.compact
-    # binding.pry
     retrieved_link = Link.find_by(original_url: original_url)
     # If the link has already been added to the db and shortened, use that link
     if retrieved_link 
-      # binding.pry
       link = retrieved_link
     else
-      # binding.pry
       short_url = keygen_source.shuffle[0,8].join
       link.update(short_url: short_url)
     end
-    # binding.pry
     link
   end
 
   def sanitize_link(link)
-    # binding.pry
     if link.original_url.include?('http://') || link.original_url.include?('https://')
-      # binding.pry
       return link
     else
-      # binding.pry
       link.original_url = "http://#{link.original_url}"
       # link.update(original_url: "http://#{link.original_url}")
     end
-    # binding.pry
     link
   end
 
